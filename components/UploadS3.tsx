925f5e0c1210c19d93672b4d7329313824ff266c
thaianhnv99 <thaianhnguyen283@gmail.com>
2023-03-17 16:03:53 +0700

    update ui

--- components/UploadS3.tsx
+++ components/UploadS3.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const UploadS3 = () => {
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false)
+
   const uploadPhoto = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0]!;
+
+    if(!file) {
+      setUrl('');
+      setLoading(false);
+      return;
+    };
+
     const filename = file.name;
     const fileType = file.type;
+    setLoading(true);
     const res = await fetch(
       `/api/upload?file=${filename}&fileType=${fileType}`
     );
@@ -14,18 +27,27 @@ const UploadS3 = () => {
     });
     if (upload.ok) {
       alert("Uploaded successfully!");
+      const s3FileUrl = `https://upload-file-s3-bucket.s3.ap-southeast-1.amazonaws.com/${filename}`;
+      console.log(s3FileUrl);
+      setUrl(s3FileUrl);
     } else {
       alert("Upload failed.");
     }
+
+    setLoading(false)
   };
 
   return (
     <>
+    <div>
       <input
         type="file"
         accept="image/png, image/jpeg"
         onChange={uploadPhoto}
       />
+      {loading && 'loading...'}
+      </div>
+      {url && <img width={500} height={500} src={url} alt={""} />}
     </>
   );
 };
